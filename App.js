58c5a60cf4a3e847f055917c9d6849d9312dec1b
subbarayudu.nidumolu <subbarayudu.nidumolu@godigit.com>
2023-07-10 17:59:55 +0530

    Product Details page added with Image Carousel and Description

--- App.js
+++ App.js
@@ -1,11 +1,13 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, View} from "react-native";
+import { StyleSheet, View } from "react-native";
 import ProductScreen from "./src/screens/ProductScreen";
+import ProductDetailsScreen from "./src/screens/ProductDetailsScreen";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <ProductScreen />
+      {/* <ProductScreen /> */}
+      <ProductDetailsScreen />
     </View>
   );
 }
@@ -14,7 +16,5 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
   },
 });
