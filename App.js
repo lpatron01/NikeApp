14a7c77886d8f16bca1b6447b42ec9a117a07788
Shubham.kadam <shubham.kadam@MacBook-Air.local>
2023-07-14 15:12:34 +0530

    Checkout page and button added

--- App.js
+++ App.js
@@ -2,12 +2,14 @@ import { StatusBar } from "expo-status-bar";
 import { StyleSheet, View } from "react-native";
 import ProductScreen from "./src/screens/ProductScreen";
 import ProductDetailsScreen from "./src/screens/ProductDetailsScreen";
+import ShoppingCart from "./src/screens/ShoppingCart";
 
 export default function App() {
   return (
     <View style={styles.container}>
       {/* <ProductScreen /> */}
-      <ProductDetailsScreen />
+      {/* <ProductDetailsScreen /> */}
+      <ShoppingCart />
     </View>
   );
 }
